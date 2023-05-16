227942e70a23082a22a8ce95f07155ec7f824887
Ghilès BADJI <ghiles.badji@gmail.com>
2023-05-16 18:15:06 +0200

    feat: create toolbar

--- porfolio/src/app/components/toolbar/toolbar.component.ts
+++ porfolio/src/app/components/toolbar/toolbar.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { ThemeSelectorComponent } from '../theme-selector/theme-selector.component';
+import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
+
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [
+    ThemeSelectorComponent,
+    LanguageSelectorComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
+  ]
 })
 export class ToolbarComponent {
 
+  @Output() sidenavToggled: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleSidenav(): void {
+    this.sidenavToggled.emit();
+  }
 }
+
+
