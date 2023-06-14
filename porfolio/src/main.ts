ae1b1d6b52ed9a82cb879215ca7ceaa5ee7f9369
Ghilès BADJI <ghiles.badji@gmail.com>
2023-06-14 16:18:55 +0200

    feat: httpclient in main.ts

--- porfolio/src/main.ts
+++ porfolio/src/main.ts
@@ -2,7 +2,13 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import { importProvidersFrom } from '@angular/core';
+
 
 bootstrapApplication(AppComponent, {
-    providers: [provideAnimations()]
+    providers: [
+        provideAnimations(),
+        importProvidersFrom(HttpClientModule)
+    ]
 });
