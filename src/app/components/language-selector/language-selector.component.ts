40b22bbd830cfb23dd4304076cac9e18106cf53c
Ghilès BADJI <ghiles.badji@gmail.com>
2023-06-24 20:04:50 +0200

    feat: creat language selector

--- src/app/components/language-selector/language-selector.component.ts
+++ src/app/components/language-selector/language-selector.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LocalStorage } from 'src/app/services/local-storage/local-storage.decorator';
+import { LanguageService } from '../../services/languauge/language.service';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @Component({
   selector: 'app-language-selector',
   templateUrl: './language-selector.component.html',
   styleUrls: ['./language-selector.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [
+    MatGridListModule,
+    
+  ]
 })
-export class LanguageSelectorComponent {
+export class LanguageSelectorComponent implements OnInit {
 
+  @LocalStorage() appLang! : 'fr-FR'|'en-US';
+
+  constructor(private readonly translateSrv: TranslateService, private readonly langService: LanguageService) { }
+
+  ngOnInit() {}
+
+  setLanguage(lang: 'fr-FR'|'en-US'): void {
+    this.appLang = lang;
+    this.langService.setLanguage(lang);
+  }
 }
